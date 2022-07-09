68ac9f2e25a30be2d59f3bc35676f0d150f511d7
GurevichSergey <spakro1802@gmail.com>
2022-07-09 22:27:32 +1000

    add all game in App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,19 @@
 package hexlet.code;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.main(args);
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        Scanner scan = new Scanner(System.in);
+        var number = scan.nextInt();
+        System.out.println("Your choice: " + number);
+        switch (number) {
+            case 1:
+                Greeting.main(args);
+                break;
+            case 2:
+                Even.main(args);
+                break;
+        }
     }
 }
