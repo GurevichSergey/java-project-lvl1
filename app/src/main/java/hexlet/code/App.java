2963cd38f0d02b48c0c5df2a4e20cac4bbfe7f98
GurevichSergey <spakro1802@gmail.com>
2022-07-14 21:43:24 +1000

    add in App game Gcd

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,15 @@
 package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.Gcd;
 import hexlet.code.games.Greeting;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc");
+        System.out.println("4 - GCD\n0 - Exit");
         Scanner scan = new Scanner(System.in);
         var number = scan.nextInt();
         System.out.println("Your choice: " + number);
@@ -21,6 +23,8 @@ public class App {
             case 3:
                 Calc.main(args);
                 break;
+            case 4:
+                Gcd.main(args);
         }
     }
 }
