6a91a947d7fb4ad7bb0689c7d0da0c97ae18ef3f
GurevichSergey <spakro1802@gmail.com>
2022-07-17 00:08:58 +1000

    Update Progression

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -2,19 +2,17 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Progression {
     public static void progression() {
-        Scanner scan = new Scanner(System.in);
         var engine = new Engine();
         engine.greeting();
         Random random = new Random();
         while (engine.round() < engine.getRoundToWin()) {
-            var arrayLength = random.nextInt();
+            var arrayLength = random.nextInt(5, 11);
             int[] array = new int[arrayLength];
-            array[0] = random.nextInt();
-            var step = random.nextInt();
+            array[0] = random.nextInt(10);
+            var step = random.nextInt(50);
             for (var i = 1; i < array.length; i++) {
                 array[i] = array[i - 1] + step;
             }
@@ -27,13 +25,12 @@ public class Progression {
             arrayString[questionNumber] = "..";
             String joined = String.join(" ", arrayString);
             System.out.println("Question: " + joined);
-            var userAnswer = scan.nextInt();
-            System.out.println("Your answer: " + userAnswer);
-            if (userAnswer == result) {
+            engine.numberUserAnswer();
+            engine.setNumberResult(result);
+            if (engine.getNumberAnswer() == result) {
                 engine.correctAnswer();
             } else {
-                System.out.print(userAnswer + " is wrong answer ;(. Correct answer was ");
-                System.out.print(array[questionNumber] + ". Let's try again, " + engine.getName());
+                engine.wrongNumberAnswer();
                 break;
             }
         }
