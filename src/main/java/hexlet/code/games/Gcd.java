3680d0c3eac0c39ff16e2242b1c843133da3319e
GurevichSergey <spakro1802@gmail.com>
2022-07-17 00:08:30 +1000

    Update Gcd

--- src/main/java/hexlet/code/games/Gcd.java
+++ src/main/java/hexlet/code/games/Gcd.java
@@ -4,22 +4,20 @@ import hexlet.code.Engine;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class Gcd {
 
     public static void gcd() {
-        Scanner scan = new Scanner(System.in);
         var engine = new Engine();
         engine.greeting();
         Random random = new Random();
         System.out.println("Find the greatest common divisor of given numbers.");
         while (engine.round() < engine.getRoundToWin()) {
-            int[] question = {random.nextInt(), random.nextInt()};
+            int[] question = {random.nextInt(50), random.nextInt(50)};
             var string = Arrays.toString(question).replace(",", "").replace("[", "").replace("]", "");
             System.out.println("Question: " + string);
-            var userAnswer = scan.nextInt();
-            System.out.println("Your answer: " + userAnswer);
+            engine.numberUserAnswer();
             var firsNumber = question[0];
             var secondNumber = question[1];
             var count = Math.min(firsNumber, secondNumber);
@@ -29,11 +27,11 @@ public class Gcd {
                     break;
                 }
             }
-            if (userAnswer == count) {
+            engine.setNumberResult(count);
+            if (engine.getNumberAnswer() == count) {
                 engine.correctAnswer();
             } else {
-                System.out.print(userAnswer + " is wrong answer ;(. Correct answer was " + count + ". ");
-                System.out.println("Let's try again, " + engine.getName());
+                engine.wrongNumberAnswer();
                 break;
             }
         }
