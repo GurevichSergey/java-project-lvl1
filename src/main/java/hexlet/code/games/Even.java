b0cc3620f958816e3e5d2b8c6d77942cc177ae9d
GurevichSergey <spakro1802@gmail.com>
2022-07-17 00:08:19 +1000

    Update Even

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -7,30 +7,28 @@ import java.util.Scanner;
 
 public class Even {
     public static void even() {
-        Scanner scan = new Scanner(System.in);
         var engine = new Engine();
         engine.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Random random = new Random();
         while (engine.round() < engine.getRoundToWin()) {
-            var randomNumber = random.nextInt();
+            var randomNumber = random.nextInt(100);
             System.out.println("Question: " + randomNumber);
-            var userAnswer = scan.next();
-            System.out.println("Your answer: " + userAnswer);
+            engine.stringUserAnswer();
             if (randomNumber % 2 == 0) {
-                if (userAnswer.equals("yes")) {
+                if (engine.getStringAnswer().equals("yes")) {
                     engine.correctAnswer();
                 } else {
-                    System.out.print(userAnswer + " is wrong answer ;(. Correct answer was 'yes'. ");
-                    System.out.println("Let's try again, " + engine.getName());
+                    engine.setStringResult("yes");
+                    engine.wrongStringAnswer();
                     break;
                 }
             } else {
-                if (userAnswer.equals("no")) {
+                if (engine.getStringAnswer().equals("no")) {
                     engine.correctAnswer();
                 } else {
-                    System.out.print(userAnswer + " is wrong answer ;(. Correct answer was 'no'. ");
-                    System.out.println("Let's try again, " + engine.getName());
+                    engine.setStringResult("no");
+                    engine.wrongStringAnswer();
                     break;
                 }
             }
