655f5cc3f0d3df5bf89259e87cc3694865e6d2b2
GurevichSergey <spakro1802@gmail.com>
2022-07-18 17:37:51 +1000

    Update Calc

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -11,13 +11,13 @@ public class Calc {
         System.out.println("What is the result of the expression?");
         Random random = new Random();
         while (engine.round() < engine.getRoundToWin()) {
-            var number1 = random.nextInt(50);
-            var number2 = random.nextInt(50);
+            var number1 = random.nextInt();
+            var number2 = random.nextInt();
             String[] array = {"*", "+", "-"};
             String operation = array[random.nextInt(array.length)];
             String question =  number1 + " " + operation + " " + number2;
             System.out.println("Question: " + question);
-            engine.numberUserAnswer();
+            engine.stringUserAnswer();
             int result = 0;
             switch (operation) {
                 case "+":
