3a857e24ea1866f542c6b49f97a091a325e71107
GurevichSergey <spakro1802@gmail.com>
2022-07-20 01:14:06 +1000

    completely rewritten 'Gcd'

--- src/main/java/hexlet/code/games/Gcd.java
+++ src/main/java/hexlet/code/games/Gcd.java
@@ -1,40 +1,39 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Interface;
+
 import java.util.Arrays;
+import java.util.Random;
 
 
-public class Gcd {
-
-    public static void gcd() {
-        var engine = new Engine();
-        engine.greeting();
-        System.out.println("Find the greatest common divisor of given numbers.");
-        while (engine.round() < engine.getRoundToWin()) {
-            int[] question = {engine.getRandomNumber(), engine.getRandomNumber()};
-            var string = Arrays.toString(question).replace(",", "").replace("[", "").replace("]", "");
-            System.out.println("Question: " + string);
-            engine.numberUserAnswer();
-            var firsNumber = question[0];
-            var secondNumber = question[1];
-            var count = Math.min(firsNumber, secondNumber);
-            for (int n = count; n >= 1; n--) {
-                if (firsNumber % n == 0 && secondNumber % n == 0) {
-                    count = n;
-                    break;
-                }
-            }
-            if (count == 0) {
-                count = Math.max(firsNumber, secondNumber);
-            }
-            engine.setNumberResult(count);
-            if (engine.getNumberAnswer() == count) {
-                engine.correctAnswer();
-            } else {
-                engine.wrongNumberAnswer();
+public class Gcd implements Interface{
+        public final String gameQuestion() {
+            return "Find the greatest common divisor of given numbers.";
+        }
+    public final String[] testAnswer() {
+        final int bound = 50;
+        Random random = new Random();
+        int[] question = {random.nextInt(bound), random.nextInt(bound)};
+        var stringQuestion = Arrays.toString(question).replace(",", "").replace("[", "").replace("]", "");
+        var firsNumber = question[0];
+        var secondNumber = question[1];
+        var answer = Math.min(firsNumber, secondNumber);
+        for (int n = answer; n >= 1; n--) {
+            if (firsNumber % n == 0 && secondNumber % n == 0) {
+                answer = n;
                 break;
             }
         }
-        engine.winMessage();
+        if (answer == 0) {
+            answer = Math.max(firsNumber, secondNumber);
+        }
+        return new String[]{stringQuestion, String.valueOf(answer)};
+    }
+    public static void game() {
+        var engine = new Engine();
+        Interface gcd = new Gcd();
+        String name = engine.greeting();
+        Engine.runGame(gcd, name);
     }
 }
