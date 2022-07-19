7bd80461083439feb4488ba73ded4d47180f62b5
GurevichSergey <spakro1802@gmail.com>
2022-07-20 01:13:41 +1000

    completely rewritten 'Calc'

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -1,44 +1,43 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Interface;
+
 import java.util.Random;
 
 
-public class Calc {
-    public static void calc() {
-        var engine = new Engine();
-        engine.greeting();
-        System.out.println("What is the result of the expression?");
+public class Calc implements Interface{
+    public final String gameQuestion() {
+        return "What is the result of the expression?";
+    }
+    public final String[] testAnswer() {
+        final int bound = 100;
         Random random = new Random();
-        while (engine.round() < engine.getRoundToWin()) {
-            var number1 = engine.getRandomNumber();
-            var number2 = engine.getRandomNumber();
-            String[] array = {"*", "+", "-"};
-            String operation = array[random.nextInt(array.length)];
-            String question =  number1 + " " + operation + " " + number2;
-            System.out.println("Question: " + question);
-            engine.numberUserAnswer();
-            int result = 0;
-            switch (operation) {
-                case "+":
-                    result = number1 + number2;
-                    break;
-                case "-":
-                    result = number1 - number2;
-                    break;
-                case "*":
-                    result = number1 * number2;
-                    break;
-                default:
-                    break;
-            }
-            engine.setNumberResult(result);
-            if (engine.getNumberAnswer() == result) {
-                engine.correctAnswer();
-            } else {
-                engine.wrongNumberAnswer();
+        var number1 = random.nextInt(bound);
+        var number2 = random.nextInt(bound);
+        String[] array = {"*", "+", "-"};
+        String operation = array[random.nextInt(array.length)];
+        String question = number1 + " " + operation + " " + number2;
+        int answer = 0;
+        switch (operation) {
+            case "+":
+                answer = number1 + number2;
                 break;
-            }
-        } engine.winMessage();
+            case "-":
+                answer = number1 - number2;
+                break;
+            case "*":
+                answer = number1 * number2;
+                break;
+            default:
+                break;
+        }
+        return new String[]{question, String.valueOf(answer)};
+    }
+    public static void game() {
+        var engine = new Engine();
+        Interface calc = new Calc();
+        String name = engine.greeting();
+        Engine.runGame(calc, name);
     }
 }
