9d6d1a9df96c0dcb5e495c174b8174350f7df77e
GurevichSergey <spakro1802@gmail.com>
2022-07-20 01:14:17 +1000

    completely rewritten 'Prime'

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -1,33 +1,33 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Interface;
+
+import java.util.Random;
 
 
-public class Prime {
-    public static void prime() {
-        var engine = new Engine();
-        engine.greeting();
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-        while (engine.round() < engine.getRoundToWin()) {
-            var number = engine.getRandomNumber();
-            System.out.println("Question: " + number);
-            engine.stringUserAnswer();
-            String result = "yes";
-            for (var i = 2; i < number / 2; i++) {
-                var check = number % i;
-                if (check == 0) {
-                    result = "no";
-                    break;
-                }
-            }
-            engine.setStringResult(result);
-            if (engine.getStringAnswer().equals(result)) {
-                engine.correctAnswer();
-            } else {
-                engine.wrongStringAnswer();
+public class Prime implements Interface {
+    public final String gameQuestion() {
+        return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    }
+    public final String[] testAnswer() {
+        final int bound = 200;
+        Random random = new Random();
+        var questionNumber = random.nextInt(bound);
+        String answer = "yes";
+        for (var i = 2; i < questionNumber / 2; i++) {
+            var check = questionNumber % i;
+            if (check == 0) {
+                answer = "no";
                 break;
             }
         }
-        engine.winMessage();
+        return new String[] {String.valueOf(questionNumber), answer};
+    }
+    public static void game() {
+        var engine = new Engine();
+        Interface prime = new Prime();
+        String name = engine.greeting();
+        Engine.runGame(prime, name);
     }
 }
