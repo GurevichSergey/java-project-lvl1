b8b81bfb9b292e8068e6ea5c19406966692798f3
GurevichSergey <spakro1802@gmail.com>
2022-07-29 17:09:08 +1000

    new logic for game 'Gcd'

--- src/main/java/hexlet/code/games/Gcd.java
+++ src/main/java/hexlet/code/games/Gcd.java
@@ -1,39 +1,37 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import hexlet.code.Interface;
 
 import java.util.Arrays;
 import java.util.Random;
 
 
-public class Gcd implements Interface {
-    public final String gameQuestion() {
-        return "Find the greatest common divisor of given numbers.";
-    }
-    public final String[] testAnswer() {
+public class Gcd {
+    public static void game() {
+        var question = "Find the greatest common divisor of given numbers.";
         final int bound = 50;
         Random random = new Random();
-        int[] question = {random.nextInt(bound), random.nextInt(bound)};
-        var stringQuestion = Arrays.toString(question).replace(",", "").replace("[", "").replace("]", "");
-        var firsNumber = question[0];
-        var secondNumber = question[1];
-        var answer = Math.min(firsNumber, secondNumber);
-        for (int n = answer; n >= 1; n--) {
-            if (firsNumber % n == 0 && secondNumber % n == 0) {
-                answer = n;
-                break;
-            }
-        }
-        if (answer == 0) {
-            answer = Math.max(firsNumber, secondNumber);
-        }
-        return new String[]{stringQuestion, String.valueOf(answer)};
-    }
-    public static void game() {
         var engine = new Engine();
-        Interface gcd = new Gcd();
-        String name = engine.greeting();
-        Engine.runGame(gcd, name);
+        String[] rightAnswer = new String[engine.getRound()];
+        String[] questionGame = new String[engine.getRound()];
+        for (var x = 0; x < engine.getRound(); x++) {
+            int[] questionNumber = {random.nextInt(bound), random.nextInt(bound)};
+            var stringQuestion = Arrays.toString(questionNumber).replace(",", "").replace("[", "").replace("]", "");
+            questionGame[x] = stringQuestion;
+            var firsNumber = questionNumber[0];
+            var secondNumber = questionNumber[1];
+            var answer = Math.min(firsNumber, secondNumber);
+            for (int n = answer; n >= 1; n--) {
+                if (firsNumber % n == 0 && secondNumber % n == 0) {
+                    answer = n;
+                    break;
+                }
+            }
+            if (answer == 0) {
+                answer = Math.max(firsNumber, secondNumber);
+            }
+            rightAnswer[x] = Integer.toString(answer);
+        }
+        Engine.runGame(rightAnswer, questionGame, question);
     }
 }
