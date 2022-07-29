c12f88ab34d77ec3724554013243f3a07e03089e
GurevichSergey <spakro1802@gmail.com>
2022-07-29 17:10:00 +1000

    new logic for game 'Engine'

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -2,34 +2,30 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Engine {
-    public static void runGame(Interface game, String name) {
-        System.out.println(game.gameQuestion());
-        final int roundToWin = 3;
-        boolean win = false;
-        for (int i = 0; i < roundToWin; i++) {
-            String[] questionAnswer = game.testAnswer();
-            System.out.println("Question: " + questionAnswer[0]);
-            System.out.print("Your answer: ");
-            Scanner scanner = new Scanner(System.in);
-            var answer = scanner.next();
-            if (answer.equals(questionAnswer[1])) {
-                System.out.println("Correct!");
-                win = true;
-            } else {
-                System.out.print(answer + " is wrong answer ;(. Correct answer was '" + questionAnswer[1] + "'. ");
-                System.out.println("Let's try again, " + name + "!");
-                break;
-            }
-        }
-        if (win) {
-            System.out.println("Congratulations, " + name + "!");
-        }
-    }
-    public final String greeting() {
+    private final int round = 3;
+    public static void runGame(String[] rightAnswer, String[] questionGame, String question) {
         System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
         Scanner scan = new Scanner(System.in);
         var name = scan.next();
+        var engine = new Engine();
         System.out.println("Hello, " + name + "!");
-        return name;
+        System.out.println(question);
+        for (int i = 0; i < engine.getRound(); i++) {
+            System.out.println("Question: " + questionGame[i]);
+            System.out.print("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
+            var answer = scanner.next();
+            if (answer.equals(rightAnswer[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.print("'" + answer + "' is wrong answer ;(. Correct answer was '" + rightAnswer[i] + "'. ");
+                System.out.println("Let's try again, " + name + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + name + "!");
+    }
+    public final int getRound() {
+        return round;
     }
 }
