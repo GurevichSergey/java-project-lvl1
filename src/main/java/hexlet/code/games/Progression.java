454c36b1f47a1c08419b8c149da1b63613e53d31
GurevichSergey <spakro1802@gmail.com>
2022-07-30 01:49:42 +1000

    update 'Progression'

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -24,10 +24,10 @@ public class Progression {
             for (var i = 0; i < arrayString.length; i++) {
                 arrayString[i] = Integer.toString(array[i]);
             }
+            var answer = arrayString[answerNumber];
             arrayString[answerNumber] = "..";
-            String joinedQuestionNumber = String.join(" ", arrayString);
-            rightAnswer[x] = arrayString[answerNumber];
-            questionGame[x] = joinedQuestionNumber;
+            rightAnswer[x] = answer;
+            questionGame[x] = String.join(" ", arrayString);
         }
         Engine.runGame(rightAnswer, questionGame, question);
     }
