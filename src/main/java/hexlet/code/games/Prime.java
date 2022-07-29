261e34644f1fe08c825ff93a14ac2c8ac3d33eb6
GurevichSergey <spakro1802@gmail.com>
2022-07-29 17:09:25 +1000

    new logic for game 'Prime'

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -1,33 +1,29 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import hexlet.code.Interface;
-
 import java.util.Random;
 
 
-public class Prime implements Interface {
-    public final String gameQuestion() {
-        return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-    }
-    public final String[] testAnswer() {
+public class Prime {
+    public static void game() {
+        var question = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         final int bound = 200;
+        var engine = new Engine();
+        String[] rightAnswer = new String[engine.getRound()];
+        String[] questionGame = new String[engine.getRound()];
         Random random = new Random();
-        var questionNumber = random.nextInt(bound);
-        String answer = "yes";
-        for (var i = 2; i < questionNumber / 2; i++) {
-            var check = questionNumber % i;
-            if (check == 0) {
-                answer = "no";
-                break;
+        for (var x = 0; x < engine.getRound(); x++) {
+            var questionNumber = random.nextInt(bound);
+            rightAnswer[x] = "yes";
+            questionGame[x] = Integer.toString(questionNumber);
+            for (var i = 2; i < questionNumber / 2; i++) {
+                var check = questionNumber % i;
+                if (check == 0) {
+                    rightAnswer[x] = "no";
+                    break;
+                }
             }
         }
-        return new String[] {String.valueOf(questionNumber), answer};
-    }
-    public static void game() {
-        var engine = new Engine();
-        Interface prime = new Prime();
-        String name = engine.greeting();
-        Engine.runGame(prime, name);
+        Engine.runGame(rightAnswer, questionGame, question);
     }
 }
