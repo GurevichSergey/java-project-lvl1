82fb10be0bbe99c8a9d6a4d13d8e318f7eb5f5f0
GurevichSergey <spakro1802@gmail.com>
2022-07-29 17:09:40 +1000

    new logic for game 'Progression'

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -1,37 +1,35 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import hexlet.code.Interface;
 
 import java.util.Random;
-public class Progression implements Interface {
-    public final String gameQuestion() {
-        return "What number is missing in the progression?";
-    }
-    public final String[] testAnswer() {
+public class Progression {
+    public static void game() {
+        var question = "What number is missing in the progression?";
         Random random = new Random();
         final var bound = 100;
-        final var randomBound = random.nextInt(5, 11);
-        int[] array = new int[randomBound];
-        array[0] = random.nextInt(bound);
-        var step = random.nextInt(bound);
-        for (var i = 1; i < array.length; i++) {
-            array[i] = array[i - 1] + step;
-        }
-        var answerNumber = random.nextInt(randomBound);
-        String[] arrayString = new String[array.length];
-        for (var x = 0; x < arrayString.length; x++) {
-            arrayString[x] = Integer.toString(array[x]);
-        }
-        var answer = arrayString[answerNumber];
-        arrayString[answerNumber] = "..";
-        String joinedQuestionNumber = String.join(" ", arrayString);
-        return new String[] {joinedQuestionNumber, answer};
-    }
-    public static void game() {
         var engine = new Engine();
-        Interface progression = new Progression();
-        String name = engine.greeting();
-        Engine.runGame(progression, name);
+        String[] rightAnswer = new String[engine.getRound()];
+        String[] questionGame = new String[engine.getRound()];
+        for (var x = 0; x < engine.getRound(); x++) {
+            final var randomBound = random.nextInt(5, 11);
+            int[] array = new int[randomBound];
+            array[0] = random.nextInt(bound);
+            var step = random.nextInt(bound);
+            for (var i = 1; i < array.length; i++) {
+                array[i] = array[i - 1] + step;
+            }
+            var answerNumber = random.nextInt(randomBound);
+            String[] arrayString = new String[array.length];
+            for (var i = 0; i < arrayString.length; i++) {
+                arrayString[i] = Integer.toString(array[i]);
+            }
+            var answer = arrayString[answerNumber];
+            arrayString[answerNumber] = "..";
+            String joinedQuestionNumber = String.join(" ", arrayString);
+            rightAnswer[x] = answer;
+            questionGame[x] = joinedQuestionNumber;
+        }
+        Engine.runGame(rightAnswer, questionGame, question);
     }
 }
