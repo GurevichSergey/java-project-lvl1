008025ba6e6a6cd83aa33e5599363a21d60e94e8
GurevichSergey <spakro1802@gmail.com>
2022-08-01 00:15:45 +1000

    add new method 'primeCheck'

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -1,29 +1,28 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.Random;
 
 
 public class Prime {
     public static void game() {
         var question = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        final int bound = 200;
-        var engine = new Engine();
-        String[] rightAnswer = new String[engine.getRound()];
-        String[] questionGame = new String[engine.getRound()];
-        Random random = new Random();
-        for (var x = 0; x < engine.getRound(); x++) {
-            var questionNumber = random.nextInt(bound);
-            rightAnswer[x] = "yes";
-            questionGame[x] = Integer.toString(questionNumber);
-            for (var i = 2; i < questionNumber / 2; i++) {
-                var check = questionNumber % i;
-                if (check == 0) {
-                    rightAnswer[x] = "no";
-                    break;
-                }
+        String[][] questionAndAnswer = new String[Engine.ROUND][2];
+        for (var x = 0; x < Engine.ROUND; x++) {
+            var questionNumber = Engine.randomBound();
+            questionAndAnswer[x][1] = primeCheck(questionNumber);
+            questionAndAnswer[x][0] = Integer.toString(questionNumber);
+        }
+        Engine.runGame(questionAndAnswer, question);
+    }
+    public static String primeCheck(int number) {
+        var answer = "yes";
+        for (var i = 2; i < number / 2; i++) {
+            var check = number % i;
+            if (check == 0) {
+                answer = "no";
+                break;
             }
         }
-        Engine.runGame(rightAnswer, questionGame, question);
+        return answer;
     }
 }
