000629402809562a3b279df2fc2bf7687f5a97e6
GurevichSergey <spakro1802@gmail.com>
2022-08-01 00:17:29 +1000

    add new method 'arrayStringBuilder'

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -1,34 +1,35 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
 import java.util.Random;
 public class Progression {
     public static void game() {
         var question = "What number is missing in the progression?";
         Random random = new Random();
-        final var bound = 100;
-        var engine = new Engine();
-        String[] rightAnswer = new String[engine.getRound()];
-        String[] questionGame = new String[engine.getRound()];
-        for (var x = 0; x < engine.getRound(); x++) {
-            final var randomBound = random.nextInt(5, 11);
-            int[] array = new int[randomBound];
-            array[0] = random.nextInt(bound);
-            var step = random.nextInt(bound);
-            for (var i = 1; i < array.length; i++) {
-                array[i] = array[i - 1] + step;
-            }
-            var answerNumber = random.nextInt(randomBound);
-            String[] arrayString = new String[array.length];
-            for (var i = 0; i < arrayString.length; i++) {
-                arrayString[i] = Integer.toString(array[i]);
-            }
+        String[][] questionAndAnswer = new String[Engine.ROUND][2];
+        for (var x = 0; x < Engine.ROUND; x++) {
+            final var arrayLength = random.nextInt(5, 11);
+            var step = Engine.randomBound();
+            var arrayString = arrayStringBuilder(step, arrayLength);
+            var answerNumber = random.nextInt(arrayLength);
             var answer = arrayString[answerNumber];
             arrayString[answerNumber] = "..";
-            rightAnswer[x] = answer;
-            questionGame[x] = String.join(" ", arrayString);
+            questionAndAnswer[x][1] = answer;
+            questionAndAnswer[x][0] = String.join(" ", arrayString);
         }
-        Engine.runGame(rightAnswer, questionGame, question);
+        Engine.runGame(questionAndAnswer, question);
+    }
+    public static String[] arrayStringBuilder(int step, int arrayLength) {
+        int[] array = new int[arrayLength];
+        array[0] = Engine.randomBound();
+        for (var i = 1; i < array.length; i++) {
+            array[i] = array[i - 1] + step;
+        }
+        String[] arrayString = new String[array.length];
+        for (var i = 0; i < arrayString.length; i++) {
+            arrayString[i] = Integer.toString(array[i]);
+        }
+        return arrayString;
     }
 }
+
