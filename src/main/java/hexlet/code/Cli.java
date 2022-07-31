8043650891eb3abaef9d49ec6bcd4dcdf1fa4800
GurevichSergey <spakro1802@gmail.com>
2022-08-01 00:13:22 +1000

    optimized 'Cli'

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void main(String[] args) {
+    public static void greet() {
         System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         Scanner scan = new Scanner(System.in);
         var name = scan.next();
