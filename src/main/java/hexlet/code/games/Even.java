0d0912556428344766be925dd49307e4af86d764
GurevichSergey <spakro1802@gmail.com>
2022-08-01 00:15:14 +1000

    add new method 'evenCheck'

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -1,25 +1,25 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.Random;
 
 public class Even {
     public static void game() {
         var question = "Answer 'yes' if number even otherwise answer 'no'.";
-        final int bound = 10000;
-        var engine = new Engine();
-        Random random = new Random();
-        String[] rightAnswer = new String[engine.getRound()];
-        String[] questionGame = new String[engine.getRound()];
-        for (var x = 0; x < engine.getRound(); x++) {
-            int questionNumber = random.nextInt(bound);
-            if (questionNumber % 2 == 0) {
-                rightAnswer[x] = "yes";
-            } else {
-                rightAnswer[x] = "no";
-            }
-            questionGame[x] = Integer.toString(questionNumber);
+        String[][] questionAndAnswer = new String[Engine.ROUND][2];
+        for (var x = 0; x < Engine.ROUND; x++) {
+            int questionNumber = Engine.randomBound();
+            questionAndAnswer[x][1] = evenCheck(questionNumber);
+            questionAndAnswer[x][0] = Integer.toString(questionNumber);
         }
-        Engine.runGame(rightAnswer, questionGame, question);
+        Engine.runGame(questionAndAnswer, question);
+    }
+    public static String evenCheck(int number) {
+        String rightAnswer;
+        if (number % 2 == 0) {
+            rightAnswer = "yes";
+        } else {
+            rightAnswer = "no";
+        }
+        return rightAnswer;
     }
 }
