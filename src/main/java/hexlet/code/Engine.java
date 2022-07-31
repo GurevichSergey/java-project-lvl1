c1ccbd1ebce13232c3c7b7970e0ad39fb84e4f7a
GurevichSergey <spakro1802@gmail.com>
2022-08-01 00:14:24 +1000

    optimized constant 'ROUND' int 'Engine'

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -1,31 +1,34 @@
 package hexlet.code;
 
+import java.util.Random;
 import java.util.Scanner;
 public class Engine {
-    private final int round = 3;
-    public static void runGame(String[] rightAnswer, String[] questionGame, String question) {
+    public static final int ROUND = 3;
+    public static void runGame(String[][] questionAndAnswer, String question) {
         System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
         Scanner scan = new Scanner(System.in);
         var name = scan.next();
-        var engine = new Engine();
         System.out.println("Hello, " + name + "!");
         System.out.println(question);
-        for (int i = 0; i < engine.getRound(); i++) {
-            System.out.println("Question: " + questionGame[i]);
+        for (int i = 0; i < ROUND; i++) {
+            System.out.println("Question: " + questionAndAnswer[i][0]);
             System.out.print("Your answer: ");
             Scanner scanner = new Scanner(System.in);
             var answer = scanner.next();
-            if (answer.equals(rightAnswer[i])) {
+            if (answer.equals(questionAndAnswer[i][1])) {
                 System.out.println("Correct!");
             } else {
-                System.out.print("'" + answer + "' is wrong answer ;(. Correct answer was '" + rightAnswer[i] + "'. ");
-                System.out.println("Let's try again, " + name + "!");
+                System.out.print("'" + answer + "' is wrong answer ;(. Correct answer was '" + questionAndAnswer[i][1]);
+                System.out.println("'. Let's try again, " + name + "!");
                 return;
             }
         }
         System.out.println("Congratulations, " + name + "!");
     }
-    public final int getRound() {
-        return round;
+    public static int randomBound() {
+        Random random = new Random();
+        final int bound = 100;
+        return random.nextInt(bound);
     }
 }
+
